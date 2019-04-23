ef1e97244d187d536f01d22d31fc1e2595feba0a
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-04-22 22:44:10 -0400

    more javascript, it almost works now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,73 +27,133 @@ const rogue = {
   counterAttack: 15
 };
 let currentPlayer;
+let currentPlayerHealth;
+let currentPlayerAttack;
+let currentPlayerAttackBase;
+let currentPlayerCounter;
 let currentDefender;
+let currentDefenderHealth;
+let currentDefenderAttack;
+let currentDefenderCounter;
 let playerSelected = false;
+let defenderSelected = false;
 //select a character
 //on click of class player
 $(".player").on("click", function() {
   if (playerSelected == false) {
-  //assigns character stats from object to currentPlayer
-  currentPlayer = this.id;
-//  switch (currentPlayer) {
- //   case wizard.name:
- //     currentPlayer = wizard;
- //     break;
-  //  case knight.name:
-  //    currentPlayer = knight;
-  //    break;
-  //  case monster.name:
-  //    currentPlayer = monster;
-  //    break;
-  //  case rogue.name:
-  //    currentPlayer = rogue;
- // }
-  //moves selected character to player area
-  let playerImage = $(`#${currentPlayer.name}`);
-  $("#player").append(playerImage);
-  //empties select a character div
-  $("#choose").detach();
-  //moves remaining characters to enemies to fight
-  $("#enemies").append($("#characters"));
-  //assigns enemy class to remaining characters
-  $("#enemies img").removeClass("player").addClass("enemy");
-  playerSelected = true;
-}
+    //assigns character stats from object to currentPlayer
+    currentPlayer = this.id;
+    switch (currentPlayer) {
+      case wizard.name:
+        currentPlayer = wizard;
+        break;
+      case knight.name:
+        currentPlayer = knight;
+        break;
+      case monster.name:
+        currentPlayer = monster;
+        break;
+      case rogue.name:
+        currentPlayer = rogue;
+    }
+    //moves selected character to player area
+    let playerImage = $(`#${currentPlayer.name}`);
+    $("#player").append(playerImage);
+    //empties select a character div
+    $("#choose").empty();
+    //moves remaining characters to enemies to fight
+    $("#enemies").append($("#characters"));
+    //assigns enemy class to remaining characters
+    $("#enemies img")
+      .removeClass("player")
+      .addClass("enemy");
+    playerSelected = true;
+    //sets current player stats
+    currentPlayerHealth = currentPlayer.health;
+    currentPlayerAttack = currentPlayer.attack;
+    currentPlayerCounter = currentPlayer.counterAttack;
+    currentPlayerAttackBase = currentPlayer.attack;
+  
+  }
 });
 
-
 //select a defender
-$(".enemy").on("click", function() {
+$("#enemies").on("click", ".enemy", function() {
   currentDefender = this.id;
- // switch (currentDefender) {
-   // case wizard.name:
-     // currentDefender = wizard;
- //     break;
- //   case knight.name:
- //     currentDefender = knight;
-   //   break;
- //   case monster.name:
-   //   currentDefender = monster;
-   //   break;
-  //  case rogue.name:
-  //    currentDefender = rogue;
+  switch (currentDefender) {
+    case wizard.name:
+      currentDefender = wizard;
+      break;
+    case knight.name:
+      currentDefender = knight;
+      break;
+    case monster.name:
+      currentDefender = monster;
+      break;
+    case rogue.name:
+      currentDefender = rogue;
   }
-  //moves image of clicked character to defender div
-  //let defenderImage = $(`#${currentDefender.name}`);
- // $("#defender").append(defenderImage);
-  //doesn't allow other enemies to be selected once defender is in defender zone
- // defenderSelected = true;
-//}
-);
+  // moves image of clicked character to defender div
+  let defenderImage = $(`#${currentDefender.name}`);
+  $("#defender").append(defenderImage);
+  //empties info from previous fight
+  $("#fight").empty();
+  // doesn't allow other enemies to be selected once defender is in defender zone
+  defenderSelected = true;
+  //sets defender stats
+  currentDefenderHealth = currentDefender.health;
+  currentDefenderAttack = currentDefender.attack;
+  currentDefenderCounter = currentDefender.counterAttack;
+});
 
 //attack button
-//if no defender, displays message that a defender must be selected
-//if defender, reduces defender health FIRST (player attack)
-//then reduces player health (defender counter attack)
-//displays message "You did ${damage} to ${defender}, reducing their health to ${defenderhealth}.
-//They counter-attacked for ${damage} damage, reducing your health to ${playerhealth}.
-//if playerHealth < 0, display lose message and display restart button
-//if defender health < 0, remove defender image and allow new defender to be selected.
-//increase playerattack by multiple of base attack
-
+$("#attack").on("click", function() {
+  //if no defender, displays message that a defender must be selected
+  if (defenderSelected === false) {
+    $("#fight").html("<p>Select a defender to attack!</p>");
+  } else {
+    //if defender, reduces defender health FIRST (player attack)
+    currentDefenderHealth = currentDefenderHealth - currentPlayerAttack;
+    //increase playerattack by multiple of base attack
+    currentPlayerAttack = currentPlayerAttack + currentPlayerAttackBase;
+    //check if defender was defeated
+    //if defender health < 1, remove defender image and allow new defender to be selected.
+    if (currentDefenderHealth < 1) {
+      defenderSelected = false;
+      $("#defender").empty();
+      $("#fight").html(
+        `<p>The ${currentDefender.name} was defeated! Select a new defender!</p>`
+      );
+    } else {
+      //reduce player health (defender counter attack)
+      currentPlayerHealth = currentPlayerHealth - currentDefenderCounter;
+      //check if current player was defeated
+      //if playerHealth < 1, display lose message and display restart button
+      if (currentPlayerHealth < 1) {
+        $("#fight").html(
+          `<p>You have been defeated! Click Restart to try again!`
+        );
+        $("#restart").html("<button>Restart</button>");
+      }
+      //displays message "You did ${damage} to ${defender}, reducing their health to ${defenderhealth}.
+      //They counter-attacked for ${damage} damage, reducing your health to ${playerhealth}.
+      else {
+        $("#fight").html(
+          `You did ${currentPlayerAttack} damage to the ${currentDefender.name}, who responded by attacking you for ${currentDefenderCounter} damage. Your health is now ${currentPlayerHealth}. Defender's health is now ${currentDefenderHealth}. Click Attack to attack again!`
+        );
+      }
+    }
+  }
+  //click handler for restart
+  $("#restart").on("click", "button", function() {
+    //moves all characters back to character div, restores heading, and removes enemy class
+    $("img").addClass("player");
+    $(".player").removeClass("enemy");
+    $("#choose").text("Choose a Character");
+    $("#characters").append(".player");
+    //resets playerSelected and defenderSelected
+    playerSelected = false;
+    defenderSelected = false;
+  });
+});
 //if enemies zone is empty, win the game, display restart option
